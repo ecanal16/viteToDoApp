34b61ef5d18ed836141b921c8f4d863f2c7c30d4
Eric Canales <ecanal16@stu.southtexascollege.edu>
2022-04-07 14:26:27 -0500

    added API

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -14,6 +14,6 @@ const add = () => {
 </script>
 
 <template>
-    <h1 class="pb-4 font-thin text-center text-6xl tracking-tight text-cyan-200">🚀 ToDo App</h1>
+    <h1 class="pb-4 font-thin text-center text-6xl tracking-tight text-cyan-200">📝 ToDo App</h1>
     <input @change="add" v-model="newTodo" type="text" class="rounded-lg text-center px-4 py-2 text-xl" placeholder="New Todo">
 </template>
